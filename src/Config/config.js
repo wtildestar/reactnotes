c834ee66489763e29d506fab323fa444964c4e15
Keith <kotikaleksei@icloud.com>
2017-11-09 21:14:50 +0300

    first commit

---
+++ src/Config/config.js
@@ -0,0 +1,8 @@
+export const DB_CONFIG = {
+    apiKey: "AIzaSyBapvFwEIC8O8hz3wjhd_MMxPTwWlkmf14",
+    authDomain: "react-notes-7e0d0.firebaseapp.com",
+    databaseURL: "https://react-notes-7e0d0.firebaseio.com",
+    projectId: "react-notes-7e0d0",
+    storageBucket: "react-notes-7e0d0.appspot.com",
+    messagingSenderId: "86187429387"
+  };
